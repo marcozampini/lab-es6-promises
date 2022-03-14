2e2a4a448fd821342382f8a4057708c2c0dfaa80
Marco Zampini <marco@bloom-it.io>
2022-03-14 17:25:04 +0100

    Revert "Iteration 2 | Make the stake with promises"
    This reverts commit f78408bff60a60663996395ca069b27e9c660059.

--- javascript/index.js
+++ javascript/index.js
@@ -54,64 +54,7 @@ getInstruction('mashedPotatoes', 0, (step0) => {
 })
 
 // Iteration 2 - using promises
-obtainInstruction('steak', 0)
-  .then((step0) => {
-    document.querySelector('#steak').innerHTML += `<li>${step0}</li>`
-  })
-  .then(
-    obtainInstruction('steak', 1).then((step1) => {
-      document.querySelector('#steak').innerHTML += `<li>${step1}</li>`
-    })
-  )
-  .then(
-    obtainInstruction('steak', 2).then((step2) => {
-      document.querySelector('#steak').innerHTML += `<li>${step2}</li>`
-    })
-  )
-  .then(
-    obtainInstruction('steak', 3).then((step3) => {
-      document.querySelector('#steak').innerHTML += `<li>${step3}</li>`
-    })
-  )
-  .then(
-    obtainInstruction('steak', 4).then((step4) => {
-      document.querySelector('#steak').innerHTML += `<li>${step4}</li>`
-    })
-  )
-  .then(
-    obtainInstruction('steak', 5).then((step5) => {
-      document.querySelector('#steak').innerHTML += `<li>${step5}</li>`
-    })
-  )
-  .then(
-    obtainInstruction('steak', 6).then((step6) => {
-      document.querySelector('#steak').innerHTML += `<li>${step6}</li>`
-    })
-  )
-  .then(
-    obtainInstruction('steak', 7).then((step7) => {
-      document.querySelector('#steak').innerHTML += `<li>${step7}</li>`
-    })
-  )
-  .then(
-    obtainInstruction('steak', 8).then((step8) => {
-      document.querySelector('#steak').innerHTML += `<li>${step8}</li>`
-    })
-  )
-
-/*
-obtainDirections(0)
-
-  .then(() => obtainDirections(1) )
-
-  .then(() => obtainDirections(2) )
-
-  .then(() => obtainDirections(3) )
-
-  .then(() => console.log("You arrived at your destination!") )
-
-  .catch((err) => console.log(err));
-  */
+// ...
 
 // Iteration 3 using async/await
 // ...
